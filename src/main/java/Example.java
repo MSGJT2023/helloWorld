cdc6a6305e29ddcea423b6da2f09190b7ca85cfe
Alex Fantana <Alex.Fantana@msg.group>
2023-07-21 14:53:39 +0300

    am adaugat salutu meu

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -23,6 +23,11 @@ public class Example {
         return new Date().toString();
     }
 
+    @RequestMapping("/hello-world/AlexFantana")
+    String date2() {
+        return "Ciaulues baules";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Example.class, args);
     }
